057d2510f7c24ccfb06976adf534ca24eb7b471f
Ukheyon Park <dkdkdkdk555@naver.com>
2022-03-26 16:03:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/attendence/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.attendence
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "환영합니다.s", Toast.LENGTH_LONG).show()
+
+    }
+}
